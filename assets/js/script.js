e627fef15545337b8b4d60dfc14ee4cf4a2c88b9
unknown <atz.softprgmr@gmail.com>
2023-12-16 17:43:06 +0500

    added changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,65 @@ function toggleMenu() {
     mobileMenu.style.display = 'none';
     overlay.style.display = 'none';
   }
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get the clickable div by its ID
+    var clickableDiv = document.querySelector('.second-tab');
+
+    // Add a click event listener to the div
+    clickableDiv.addEventListener('click', function() {
+        // Redirect to another page when the div is clicked
+        window.location.href = 'https://aitizaz-ul-haq.github.io/phi-consulting-website/sub_pages/buisness_consulting.html'; 
+        // window.location.href = 'http://127.0.0.1:5500/sub_pages/buisness_consulting.html'; 
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the clickable div by its ID
+  var clickableDiv = document.querySelector('.third-tab');
+
+  // Add a click event listener to the div
+  clickableDiv.addEventListener('click', function() {
+      // Redirect to another page when the div is clicked
+      window.location.href = 'https://aitizaz-ul-haq.github.io/phi-consulting-website/sub_pages/customer_experience_consulting.html'; 
+      // window.location.href = 'http://127.0.0.1:5500/sub_pages/customer_experience_consulting.html'; 
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the clickable div by its ID
+  var clickableDiv = document.querySelector('.fourth-tab');
+
+  // Add a click event listener to the div
+  clickableDiv.addEventListener('click', function() {
+      // Redirect to another page when the div is clicked
+      window.location.href = 'https://aitizaz-ul-haq.github.io/phi-consulting-website/sub_pages/financial_consulting.html'; 
+      // window.location.href = 'http://127.0.0.1:5500/sub_pages/financial_consulting.html'; 
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the clickable div by its ID
+  var clickableDiv = document.querySelector('.fifth-tab');
+
+  // Add a click event listener to the div
+  clickableDiv.addEventListener('click', function() {
+      // Redirect to another page when the div is clicked
+      window.location.href = 'https://aitizaz-ul-haq.github.io/phi-consulting-website/sub_pages/hr_and_recruitment_consulting.html'; 
+      // window.location.href = 'http://127.0.0.1:5500/sub_pages/hr_and_recruitment_consulting.html'; 
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the clickable div by its ID
+  var clickableDiv = document.querySelector('.sixth-tab');
+
+  // Add a click event listener to the div
+  clickableDiv.addEventListener('click', function() {
+      // Redirect to another page when the div is clicked
+      window.location.href = 'https://aitizaz-ul-haq.github.io/phi-consulting-website/sub_pages/sales_consulting.html'; 
+      // window.location.href = 'http://127.0.0.1:5500/sub_pages/sales_consulting.html'; 
+  });
+});
   
